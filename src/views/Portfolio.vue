fb39f9654eec64d52fed605986376d63c51a1649
Joy <greeniejoy@gmail.com>
2020-10-06 22:57:38 -0500

    set up portfolio template

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,5 +1,54 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="portfolio">
+    <v-containter>
+      <!-- <h1>Joy's awesome portfolio!</h1> -->
+      <v-spacer class="ma-6"></v-spacer>
+      <v-row class="mb-7" no-gutters>
+        <v-col md="6" offset-md="3">
+          <v-card class="pa-2" outlined tile>
+            <v-row>
+              <v-col class="ma-3" md="3">
+                <v-img
+                  class="rounded"
+                  max-height="350"
+                  max-width="100"
+                  src="../assets/diveBandGig.png"
+                ></v-img>
+              </v-col>
+              <v-col class="blue-grey--text text--darken-2">
+                <v-card-text>
+                  <h1>Dive</h1>
+                  <v-spacer class="ma-2"></v-spacer>
+                  <h3>
+                    Dive is a mobile application....blablablablablabalblabla
+                  </h3>
+                </v-card-text>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row class="mb-6" no-gutters>
+        <v-col md="6" offset-md="3">
+          <v-card class="pa-2" outlined tile>
+            <v-row>
+              <v-col class="ma-3" md="3">
+                <v-img
+                  class="rounded"
+                  max-height="350"
+                  max-width="100"
+                  src="../assets/diveBandGig.png"
+                ></v-img>
+              </v-col>
+              <v-col class="blue-grey--text text--darken-2">
+                <v-card-text>
+                  <h1>Dive</h1>
+                </v-card-text>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-containter>
   </div>
 </template>
